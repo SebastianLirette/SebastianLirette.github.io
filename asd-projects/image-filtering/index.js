0516e77f6f23a69f046d3de5461ee5b11e6a522f
SebastianLirette <sebastianlirette@gmail.com>
2023-06-15 16:38:00 +0000

    saving image filtering

--- asd-projects/image-filtering/index.js
+++ asd-projects/image-filtering/index.js
@@ -20,7 +20,7 @@ function resetAndRender() {
 // all of your apply functions
 function applyAndRender() {
   // Multiple TODOs: Call your apply function(s) here
-  applyFilter(reddify, decreaseBlue);
+  applyFilter(reddify);
   applyFilterNoBackground(decreaseBlue);
   applyFilterNoBackground(increaseGreenByBlue);
   // do not change the below line of code
