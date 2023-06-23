07e4fbc7bf6df1eda1ea2504f1d4e7dbe6a14ab4
SebastianLirette <sebastianlirette@gmail.com>
2023-06-23 16:41:16 +0000

    saving sorting exercise

--- asd-projects/sorting-exercise/index.js
+++ asd-projects/sorting-exercise/index.js
@@ -16,9 +16,9 @@ The CSS ids you will work with are:
 // TODO 2: Implement bubbleSort
 async function bubbleSort(array) {
     for (var i = 0; i <= array.length - 1; i++) {
-        for (var j = array.length - 1; j <= i + 1; j++) {
+        for (var j = array.length - 1; j >= i + 1; j--) {
             if (array[j].value < array[j - 1].value) {
-                swap(array, array[j], array[j - 1]);
+                swap(array, j, j - 1);
                 updateCounter(bubbleCounter);
                 await sleep();
             }
@@ -27,10 +27,37 @@ async function bubbleSort(array) {
 }
 
 // TODO 3: Implement quickSort
+async function quickSort(array, left, right) {
+    if ((left - right) > 0) {
+        var index = await partition(array, left, right)
+        if (left < index - 1) {
+            await quickSort(array, left, index - 1)
+            if (right > index) {
+                await quickSort(array, index, right)
+            }
+        }
+    }
+}
 
 
 // TODOs 4 & 5: Implement partition
-
+async function partition(array, left, right) {
+    var pivot = array[Math.floor((right + left) / 2)].value;
+    while (left < right) {
+        while (left < pivot) {
+            left++
+        }
+        while (right > pivot) {
+            right--
+        }
+        if (left < right) {
+            swap(array, left, right)
+            updateCounter(quickCounter)
+            await sleep()
+        }
+    }
+    return left + 1
+}
 
 // TODO 1: Implement swap
 function swap(array, i, j) {
